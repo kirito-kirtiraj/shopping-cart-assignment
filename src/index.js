ca4d498ce7bc86b997d0a6c68165442a1f84b0bf
Kirtiraj <officialkirtiraj@gmail.com>
2021-07-28 19:06:17 +0530

    Loading animation added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import "./styles/main.scss";
 
 import headerSetup from "./components/Header";
 import createCategorySection from "./components/Category";
+import { startSpinner, stopSpinner } from "./components/LoadingSpinner";
 import APICall from "./APIs/API";
 
 headerSetup();
 
 const main = document.querySelector("main");
+
+startSpinner(main);
+
 const response = APICall.getData("/categories");
 response.then((categories) => {
+    stopSpinner();
     categories.sort((a, b) =>
         a.order > b.order ? 1 : b.order > a.order ? -1 : 0
     );
